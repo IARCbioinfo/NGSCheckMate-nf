d2e1b446651321baa84ebaf6ef52be67cbac732b
nalcala <alcalan@fellows.iarc.fr>
2019-10-28 17:51:00 +0100

    use local version of ngscheckmate2xgmml.R

--- bin/plots.R
+++ bin/plots.R
@@ -1,11 +1,8 @@
 args = commandArgs(trailingOnly=TRUE)
-NCM_folder = Sys.getenv("NCM_HOME") #args[1]
 label.file = args[1]
-
-print(c(NCM_folder,label.file) )
 
 ngscheckmateoutput.file = "NCM_output/output_all.txt"
 
-source(paste0(NCM_folder,"/graph/ngscheckmate2xgmml.R") )
+source("ngscheckmate2xgmml.R")
 create.xgmml.from.ngscheckmateout(label.file,ngscheckmateoutput.file,"NCM_graph_wrongmatch.xgmml")
 create.xgmml.from.ngscheckmateout(label.file,ngscheckmateoutput.file,"NCM_graph.xgmml",filter=FALSE)
