b38ab191faa82b0582e5ed00bcff3af4c1777cc4
nalcala <alcalan@fellows.iarc.fr>
2019-10-28 17:41:06 +0100

    change output file name

--- bin/plots.R
+++ bin/plots.R
@@ -8,4 +8,4 @@ ngscheckmateoutput.file = "NCM_output/output_all.txt"
 
 source(paste0(NCM_folder,"/graph/ngscheckmate2xgmml.R") )
 create.xgmml.from.ngscheckmateout(label.file,ngscheckmateoutput.file,"NCM_graph_wrongmatch.xgmml")
-create.xgmml.from.ngscheckmateout(label.file,ngscheckmateoutput.file,"NCM_graph_wrongmatch.xgmml",filter=FALSE)
+create.xgmml.from.ngscheckmateout(label.file,ngscheckmateoutput.file,"NCM_graph.xgmml",filter=FALSE)
